d66c29211e42ee41655dc7221df2dee96d882bfa
max963 <mg_morais@id.uff.br>
2021-03-31 00:09:13 -0300

    feat: gerando token

--- TarefasBackEnd/Controllers/UsuarioController.cs
+++ TarefasBackEnd/Controllers/UsuarioController.cs
@@ -1,5 +1,9 @@
 using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using TarefasBackEnd.Dtos;
 using TarefasBackEnd.models;
 using TarefasBackEnd.Repositories;
@@ -40,7 +44,10 @@ namespace TarefasBackEnd.Controllers
                 return BadRequest();
             }
 
-            return Ok(usuario);
+            return Ok(new {
+                usuario = usuario,
+                token = GenerateToken(usuario)
+            });
         }
 
         [HttpPut]
@@ -61,6 +68,29 @@ namespace TarefasBackEnd.Controllers
             repository.Delete(Id);
 
             return Ok();
+
+        }
+
+
+        private string GenerateToken(Usuario usuario) {
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = "123456asdksdfsdaiusdsdbnxnzxbiuwqnxjnksmncxhsmxzkiyhermmxj";
+            var descriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]{
+                    new Claim(ClaimTypes.Name, usuario.Id.ToString()),
+                }),
+                Expires = DateTime.UtcNow.AddHours(3),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)), SecurityAlgorithms.HmacSha256Signature
+                )
+
+            };
+
+            var token = tokenHandler.CreateToken(descriptor);
+
+            return tokenHandler.WriteToken(token);
 
         }
     }
